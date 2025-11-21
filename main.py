5ed957328a77640803e4a21d1d3c92ed280ae39e
Kevin Doshi <kevin.doshi2@spit.ac.in>
2025-11-20 21:46:42 -0500

    first commit

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ app = Flask(__name__)
 
 
 # load databasedataset===================================
-sym_des = pd.read_csv("datasets/symtoms_df.csv")
-precautions = pd.read_csv("datasets/precautions_df.csv")
-workout = pd.read_csv("datasets/workout_df.csv")
-description = pd.read_csv("datasets/description.csv")
-medications = pd.read_csv('datasets/medications.csv')
-diets = pd.read_csv("datasets/diets.csv")
+sym_des = pd.read_csv("dataset/symtoms_df.csv")
+precautions = pd.read_csv("dataset/precautions_df.csv")
+workout = pd.read_csv("dataset/workout_df.csv")
+description = pd.read_csv("dataset/description.csv")
+medications = pd.read_csv('dataset/medications.csv')
+diets = pd.read_csv("dataset/diets.csv")
 
 
 # load model===========================================
